ab1f7307435167172ff487b620d2bdaaf7737718
XingLi <xing@evestemptation.com>
2018-03-30 15:10:39 -0700

    add search form component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
